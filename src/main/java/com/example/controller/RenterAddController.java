74d2161b3f7295739339f8b1d7d56d8ecfaa632c
huy <huy.pd.2045@aptechlearning.edu.vn>
2023-01-04 20:43:38 +0700

    tony update

---
+++ src/main/java/com/example/controller/RenterAddController.java
@@ -0,0 +1,49 @@
+package com.example.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
+public class RenterAddController {
+
+
+
+        @FXML
+        private Button NewRenterADD;
+
+        @FXML
+        private TextField NewRenterAddress;
+
+        @FXML
+        private ComboBox<?> NewRenterCommune;
+
+        @FXML
+        private DatePicker NewRenterDOB;
+
+        @FXML
+        private ComboBox<?> NewRenterDistric;
+
+        @FXML
+        private HBox NewRenterEmail;
+
+        @FXML
+        private ComboBox<?> NewRenterGender;
+
+        @FXML
+        private TextField NewRenterName;
+
+        @FXML
+        private HBox NewRenterPhone;
+
+        @FXML
+        private ComboBox<?> NewRenterProvince;
+
+        @FXML
+        private Button NewRenterReset;
+
+}
+
+
